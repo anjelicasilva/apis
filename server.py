94ec873a4cf83a23801ab5f158b35936455aff56
Your Name <you@example.com>
2020-01-09 20:07:55 -0800

    Zipcode search works

--- server.py
+++ server.py
@@ -42,7 +42,11 @@ def find_afterparties():
     # TODO: Make a request to the Event Search endpoint to search for events
     #
     # - Use form data from the user to populate any search parameters
-    #
+    payload['postalCode'] = request.args.get('search-zip')
+    response = requests.get(url, params=payload)
+    data = response.json()
+    events = data['_embedded']['events']
+
     # - Make sure to save the JSON data from the response to the `data`
     #   variable so that it can display on the page. This is useful for
     #   debugging purposes!
@@ -50,8 +54,8 @@ def find_afterparties():
     # - Replace the empty list in `events` with the list of events from your
     #   search results
 
-    data = {'Test': ['This is just some test data']}
-    events = []
+    # data = {'Test': ['This is just some test data']}
+    # events = []
 
     return render_template('afterparty-search-results.html',
                            pformat=pformat,
